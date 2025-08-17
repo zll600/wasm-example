1fa1c7f6f79925b02435b8a666cfa8047f3c5817
zll600 <3400692417@qq.com>
2025-08-17 23:07:32 +0800

    webassembly linear memory

--- index.js
+++ index.js
@@ -3,14 +3,44 @@
 import init from "./pkg/wasm_example.js";
 
 const runWasm = async () => {
-  // Instantiate our wasm module
-  const helloWorld = await init("./pkg/wasm_example_bg.wasm");
+  const rustWasm = await init("./pkg/wasm_example_bg.wasm");
 
-  // Call the Add function export from wasm, save the result
-  const result = helloWorld.call_me_from_javascript(24, 24);
+  /**
+   * Part one: Write in Wasm, Read in JS
+   */
+  console.log("Write in Wasm, Read in JS, Index 0:");
 
-  console.log(result); // Should output '72'
-  console.log(helloWorld.ADD_CONSTANT); // Should output 'undefined'
-  console.log(helloWorld.add_integer_with_constant); // Should output 'undefined'
+  // First, let's have wasm write to our buffer
+  rustWasm.store_value_in_wasm_memory_buffer_index_zero(24);
+
+  // Next, let's create a Uint8Array of our wasm memory
+  let wasmMemory = new Uint8Array(rustWasm.memory.buffer);
+
+  // Then, let's get the pointer to our buffer that is within wasmMemory
+  let bufferPointer = rustWasm.get_wasm_memory_buffer_pointer();
+
+  // Then, let's read the written value at index zero of the buffer,
+  // by accessing the index of wasmMemory[bufferPointer + bufferIndex]
+  console.log(wasmMemory[bufferPointer + 0]); // Should log "24"
+
+  /**
+   * Part two: Write in JS, Read in Wasm
+   */
+  console.log("Write in JS, Read in Wasm, Index 1:");
+
+  // First, let's write to index one of our buffer
+  wasmMemory[bufferPointer + 1] = 15;
+
+  // Then, let's have wasm read index one of the buffer,
+  // and return the result
+  console.log(rustWasm.read_wasm_memory_buffer_and_return_index_one()); // Should log "15"
+
+  /**
+   * NOTE: if we were to continue reading and writing memory,
+   * depending on how the memory is grown by rust, you may have
+   * to re-create the Uint8Array since memory layout could change.
+   * For example, `let wasmMemory = new Uint8Array(rustWasm.memory.buffer);`
+   * In this example, we did not, but be aware this may happen :)
+   */
 };
 runWasm();
